f14ddaa6ba6f55d15a3fe598dd08c9abe0b0e384
isa <izzy.mngd@gmail.com>
2026-06-20 00:53:39 +0800

    removed summarease project and added graphic design updates

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,8 +10,6 @@ import tick5 from "../assets/project-images/tick-5.png";
 import rewear1 from "../assets/project-images/rewear-1.png";
 import rewear2 from "../assets/project-images/rewear-2.png";
 import rewear3 from "../assets/project-images/rewear-3.png";
-import summarease1 from "../assets/project-images/summarease.png";
-import summarease2 from "../assets/project-images/summarease-2.png";
 import pokemonQuiz1 from "../assets/project-images/pokemon-quiz-1.png";
 import pokemonQuiz2 from "../assets/project-images/pokemon-quiz-2.png";
 import resumate1 from "../assets/project-images/resumate-1.png";
@@ -21,10 +19,12 @@ import silksong1 from "../assets/project-images/silksong-1.png";
 import silksong2 from "../assets/project-images/silksong-2.png";
 import silksong3 from "../assets/project-images/silksong-3.png";
 import silksong4 from "../assets/project-images/silksong-4.png";
-import nailGraphicDesign1 from "../assets/graphic-design-images/nail 1.png";
-import nailGraphicDesign2 from "../assets/graphic-design-images/nail 2.png";
-import twixGraphicDesign from "../assets/graphic-design-images/twix.png";
-import ediblesLogoDesign from "../assets/graphic-design-images/edibles logo.png";
+import hiringGraphicDesign from "../assets/graphic-design-images/hiring.webp";
+import menuGraphicDesign from "../assets/graphic-design-images/new menu.webp";
+import nailGraphicDesign1 from "../assets/graphic-design-images/nail design 1.webp";
+import nailGraphicDesign2 from "../assets/graphic-design-images/nail design 2.webp";
+import twixGraphicDesign from "../assets/graphic-design-images/twix.webp";
+import ediblesLogoDesign from "../assets/graphic-design-images/edibles logo.webp";
 import cLogoDesign from "../assets/graphic-design-images/C.png";
 
 type ProjectCardProps = {
@@ -109,121 +109,113 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   );
 };
 
+const rewearImages = [rewear1, rewear2, rewear3];
+
+const pokemonImages = [pokemonQuiz1, pokemonQuiz2];
+
+const tickImages = [tick1, tick2, tick3, tick4, tick5];
+
+const resumateImages = [resumate1, resumate2, resumate3];
+
+const silksongImages = [silksong1, silksong2, silksong3, silksong4];
+
+const projects: ProjectCardProps[] = [
+  {
+    title: "resumate",
+    link: "https://resumate-beta.vercel.app/",
+    ImageComponent: <ImageCarousel images={resumateImages} />,
+    descriptions: [
+      "A website to help job-seekers create their resumes easily",
+      "A resume builder with templates to choose from",
+      "Includes an AI-powered interview simulator to help users prepare",
+      "Resumes are built utilizing the python-docx library",
+    ],
+    key_features: [
+      "fill out a simple form to generate a resume instantly",
+      "save resume to docx",
+      "AI powered interview simulator",
+    ],
+    stack: [
+      "React",
+      "React Router",
+      "Tailwindcss",
+      "python-docx",
+      "Django",
+      "Django REST framework",
+    ],
+  },
+  {
+    title: "Re:wear",
+    link: "https://rewear-x2gd.onrender.com/",
+    ImageComponent: <ImageCarousel images={rewearImages} />,
+    descriptions: [
+      "A buy and sell marketplace",
+      "Built with Django + React, featuring full CRUD functionality, user auth, and image uploads via Supabase",
+    ],
+    key_features: [
+      "user registration & login with JWT-based authentication",
+      "route-based navigation using React Router",
+      "item listings with image upload (Supabase storage)",
+      "RESTful API with Django REST framework",
+    ],
+    stack: [
+      "React",
+      "React Router",
+      "Bootstrap",
+      "Django",
+      "Django REST framework",
+      "Django simple-jwt",
+    ],
+  },
+  {
+    title: "tick",
+    link: "https://tick-ai.vercel.app/",
+    ImageComponent: <ImageCarousel images={tickImages} />,
+    descriptions: [
+      "AI-Powered To Do List",
+      "Built with React, integrated Clerk for user auth, and used Appwrite's SDK for database management. Also allows users to generate tasks with AI",
+    ],
+    key_features: [
+      "creating, organizing, and tracking tasks with due dates",
+      "integrated Gemini AI to allow users to generate tasks from a prompt",
+      "item listings with image upload (Supabase storage)",
+    ],
+    stack: ["React", "React Router", "Bootstrap", "Appwrite", "Clerk"],
+  },
+  {
+    title: "Hollow Knight: Silksong - landing page",
+    link: "https://hollowknightsilksong-fan.vercel.app/",
+    ImageComponent: <ImageCarousel images={silksongImages} />,
+    descriptions: [""],
+    stack: ["React", "Tailwindcss"],
+  },
+  {
+    title: "pokemon quiz",
+    link: "https://pokemon-quiz-a3i5.onrender.com/",
+    ImageComponent: <ImageCarousel images={pokemonImages} />,
+    descriptions: ["A quiz game i built for fun"],
+    key_features: [
+      "multiple-choice quiz with randomized questions",
+      "final score + result screen at the end",
+      "leaderboard for displaying scores of players",
+    ],
+    stack: ["React", "React Router", "Bootstrap"],
+  },
+];
+
 const Projects = () => {
-  const rewearImages = [rewear1, rewear2, rewear3];
-
-  const summareaseImages = [summarease1, summarease2];
-
-  const pokemonImages = [pokemonQuiz1, pokemonQuiz2];
-
-  const tickImages = [tick1, tick2, tick3, tick4, tick5];
-
-  const resumateImages = [resumate1, resumate2, resumate3];
-
-  const silksongImages = [silksong1, silksong2, silksong3, silksong4];
-
-  const projects: ProjectCardProps[] = [
-    {
-      title: "resumate",
-      link: "https://resumate-beta.vercel.app/",
-      ImageComponent: <ImageCarousel images={resumateImages} />,
-      descriptions: [
-        "A website to help job-seekers create their resumes easily",
-        "A resume builder with templates to choose from",
-        "Includes an AI-powered interview simulator to help users prepare",
-        "Resumes are built utilizing the python-docx library",
-      ],
-      key_features: [
-        "fill out a simple form to generate a resume instantly",
-        "save resume to docx",
-        "AI powered interview simulator",
-      ],
-      stack: [
-        "React",
-        "React Router",
-        "Tailwindcss",
-        "python-docx",
-        "Django",
-        "Django REST framework",
-      ],
-    },
-    {
-      title: "Re:wear",
-      link: "https://rewear-x2gd.onrender.com/",
-      ImageComponent: <ImageCarousel images={rewearImages} />,
-      descriptions: [
-        "A buy and sell marketplace",
-        "Built with Django + React, featuring full CRUD functionality, user auth, and image uploads via Supabase",
-      ],
-      key_features: [
-        "user registration & login with JWT-based authentication",
-        "route-based navigation using React Router",
-        "item listings with image upload (Supabase storage)",
-        "RESTful API with Django REST framework",
-      ],
-      stack: [
-        "React",
-        "React Router",
-        "Bootstrap",
-        "Django",
-        "Django REST framework",
-        "Django simple-jwt",
-      ],
-    },
-    {
-      title: "tick",
-      link: "https://tick-ai.vercel.app/",
-      ImageComponent: <ImageCarousel images={tickImages} />,
-      descriptions: [
-        "AI-Powered To Do List",
-        "Built with React, integrated Clerk for user auth, and used Appwrite's SDK for database management. Also allows users to generate tasks with AI",
-      ],
-      key_features: [
-        "creating, organizing, and tracking tasks with due dates",
-        "integrated Gemini AI to allow users to generate tasks from a prompt",
-        "item listings with image upload (Supabase storage)",
-      ],
-      stack: ["React", "React Router", "Bootstrap", "Appwrite", "Clerk"],
-    },
-    {
-      title: "Hollow Knight: Silksong - landing page",
-      link: "https://hollowknightsilksong-fan.vercel.app/",
-      ImageComponent: <ImageCarousel images={silksongImages} />,
-      descriptions: [""],
-      stack: ["React", "Tailwindcss"],
-    },
-    {
-      title: "summarease",
-      link: "https://summarease-bsyf.onrender.com",
-      ImageComponent: <ImageCarousel images={summareaseImages} />,
-      descriptions: [
-        "A text summarization tool for long-form content",
-        "Integrated Gemini AI for AI powered text summarization",
-        "Initially built with node.js + express.js",
-      ],
-      key_features: ["AI powered summary generator", "export to PDF support"],
-      stack: ["HTML", "CSS", "Node.js", "Express.js", "Gemini API"],
-    },
-    {
-      title: "pokemon quiz",
-      link: "https://pokemon-quiz-a3i5.onrender.com/",
-      ImageComponent: <ImageCarousel images={pokemonImages} />,
-      descriptions: ["A quiz game i built for fun"],
-      key_features: [
-        "multiple-choice quiz with randomized questions",
-        "final score + result screen at the end",
-        "leaderboard for displaying scores of players",
-      ],
-      stack: ["React", "React Router", "Bootstrap"],
-    },
-  ];
-
   return (
     <div>
       <div className="mb-10">
         <h2 className="font-semibold text-3xl my-4">Graphic Designs</h2>
         <div className="bg-gray-100 p-4 rounded">
           <div className="grid gap-y-6 md:grid-cols-2 md:gap-4">
+            <div>
+              <img src={hiringGraphicDesign} alt="Hiring graphic design" />
+            </div>
+            <div>
+              <img src={menuGraphicDesign} alt="Menu graphic design" />
+            </div>
             <div>
               <img src={nailGraphicDesign2} alt="Nail Graphic Design 1" />
             </div>
